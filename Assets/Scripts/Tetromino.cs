0e1acdb25615285731e74dfeaded30347ebf8e4b
Skorpi76 <skorpi76@gmail.com>
2017-03-07 22:08:20 -0400

    Tetromino Movement
    Movement Added

--- Assets/Scripts/Tetromino.cs
+++ Assets/Scripts/Tetromino.cs
@@ -3,14 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Tetromino : MonoBehaviour {
+    float fall = 0;
+    public float fallSpeed = 1;
 
-	// Use this for initialization
 	void Start () {
 		
 	}
 	
-	// Update is called once per frame
+
 	void Update () {
-		
+        CheckUserInput();
 	}
+
+
+
+    void CheckUserInput()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(1f, 0f, 0f);
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.position += new Vector3(-1f, 0f, 0f);
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.Rotate(0, 0, 90); 
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow) || Time.time - fall >= fallSpeed)
+        {                                                                    
+            transform.position += new Vector3(0f, -1f, 0f);
+            fall = Time.time;
+        }
+    }
 }
